d579db187cd835ffae5be7be02232e894fb0ee27
Gaston Garcia <ggarcia@truelogicsoftware.com[alias]>
2017-06-23 13:43:18 -0300

    Marvel item link

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../assets/stylesheets/Item.scss';
 
 const Item = ({item, type}) => {
   let name = (type === 'comics' || type === 'series' || type === 'events') ? item.title : (type === 'creators') ? item.fullName : item.name;
   return (
-      <a href={'/' + type + '/' + item.id} className="item pull-left" title={item.name}>
+      <Link to={'/' + type + '/' + item.id} className="item pull-left" title={item.name}>
         <h4>{ name }</h4>
         <img className='image_available' src={item.thumbnail.path + '/portrait_incredible.jpg'} alt={name}/>
         <img className='image_not_available' src="http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_incredible.jpg" alt={name}/>
-      </a>
+      </Link>
   )
 }
 
